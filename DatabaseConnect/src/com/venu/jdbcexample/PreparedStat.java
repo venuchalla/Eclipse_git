8352ff95be9978ac18714a42598f2f81bd880a1d
venuchalla <venuchalla1993@gmail.com>
2018-06-26 12:12:00 -0500

    Update PreparedStat.java

--- DatabaseConnect/src/com/venu/jdbcexample/PreparedStat.java
+++ DatabaseConnect/src/com/venu/jdbcexample/PreparedStat.java
@@ -8,8 +8,27 @@ import java.sql.Statement;
 public class PreparedStat{
 
 	public static void main(String[] args) {
-		 Connection connection=null;
-		 PreparedStatement preparedStatement = null;
+		try{  
+Class.forName("org.postgresql.Driver");  
+  
+Connection con=DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres","venu");  
+  
+PreparedStatement stmt=con.prepareStatement("insert into company values(?,?,?,?,?)");  
+stmt.setInt(1,101);//1 specifies the first parameter in the query  
+stmt.setString(2,"Ratan"); 
+stmt.setInt(3,43);
+stmt.setString("hyderabad");
+stmt.setBigdecimal(20000.00d);
+  
+int i=stmt.executeUpdate();  
+System.out.println(i+" records inserted");  
+  
+con.close();  
+  
+}catch(Exception e){ System.out.println(e);}  
+  
+}  
+}  
 
 	}
 
